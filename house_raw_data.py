6248175b70b9f2c33cd36e63956461cf08eee091
estelladai <yowyowdai@gmail.com>
2023-06-08 16:07:06 +0800

    updated

--- house_raw_data.py
+++ house_raw_data.py
@@ -21,33 +21,36 @@ def real_estate_crawler(year, season):
     res = requests.get("https://plvr.land.moi.gov.tw//DownloadSeason?season=" + str(year) + "S" + str(
         season) + "&type=zip&fileName=lvr_landcsv.zip")
 
-    # save content to file
-    fname = str(year) + str(season) + '.zip'
-    open(fname, 'wb').write(res.content)
-
     # make additional folder for files to extract
     folder = 'real_estate' + str(year) + str(season)
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
+    folder_path = os.path.join(os.getcwd(), 'raw_data', folder)
+    if not os.path.isdir(folder_path):
+        os.mkdir(folder_path)
+
+    # save content to file
+    fname = str(year) + str(season) + '.zip'
+    file_path = os.path.join(folder_path, fname)
+    open(file_path, 'wb').write(res.content)
 
     # extract files to the folder
-    with zipfile.ZipFile(fname, 'r') as zip_ref:
-        zip_ref.extractall(folder)
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(folder_path)
 
     time.sleep(10)
 
 
-# save to directory
-os.chdir('./raw_data')
+def download_raw_data():
+    start_year = 106
+    end_year = 113
 
-start_year = 106
-end_year = 113
+    # year資料取出106~112年
+    for year in range(start_year, end_year):
+        for season in range(1, 5):
+            # season資料取到112年Q1
+            if year == 112 and season > 1:
+                break
+            real_estate_crawler(year, season)
+            print(f'downloaded year:{year},season:{season}')
 
-# year資料取出106~112年
-for year in range(start_year, end_year):
-    for season in range(1, 5):
-        # season資料取出112年Q1
-        if year == 112 and season > 1:
-            break
-        real_estate_crawler(year, season)
-        print(f'downloaded year:{year},season:{season}')
+
+download_raw_data()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from rent_roi import *
+from house_raw_data import *
 from make_chart import *
+from rent_roi import *
 
 if __name__ == "__main__":
+    download_raw_data()
     make_plt_chart()
     combine_rent_price_roi_2023()
     rent_price_roi_df2gsheet()
